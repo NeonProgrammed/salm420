c4602a2b9e9fffa2a7801b45728d56bb44d92e5f
NeonProgrammed <bowersg45@gmail.com>
2018-02-04 01:47:06 -0500

    Cursor
    Created gameObject that changed the cursor icon. It looked really small on my screen but I think it's just because of my surface's resolution. Will investigate later -GB

---
+++ dndgame/Assets/Scripts/Global scripts/ChangeMouseIcon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMouseIcon : MonoBehaviour {
+
+    public Texture2D dragon_Icon;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.SetCursor(dragon_Icon, Vector2.zero, CursorMode.Auto);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
