81f57ea94bfdf67eb7f8f6eaefea706686e207fc
NeonProgrammed <bowersg45@gmail.com>
2018-02-09 00:04:11 -0500

    Creating io scripts
    Starting development of file io system. Will go over with Bryce and Harold how it works so we can structure the .suc and .lnl files. May also restructure game files for github to avoid future problems uploading to repo... I'll get back to that. Couldn't figure out how to get it to read or write ... Will have to work it out on desktop so I can take a better look at it. Pushing to repo

---
+++ dndgame/Assets/Scripts/IO scripts/FileIO.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+//Learning file io that unity uses. Will refactor for our uses
+
+public class FileIO : MonoBehaviour {
+
+    string readpath;
+    string writepath;
+
+    public List<string> stringList = new List<string>();
+    public List<string> writeList = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+        readpath = Application.dataPath + "/ReadFile.txt";
+        writepath = Application.dataPath + "/WriteFile.txt";
+
+        ReadFile(readpath);
+        WriteFile(writepath);
+        AppendFile(writepath);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Read the file
+    void ReadFile(string filePath)
+    {
+        StreamReader sReader = new StreamReader(filePath);
+
+        //While we are still reading from the file
+        while (!sReader.EndOfStream)
+        {
+            string line = sReader.ReadLine();
+            //string line = sReader.ReadLine();
+            stringList.Add(line);
+        }
+
+        sReader.Close(); //Closes the file
+    }
+
+    //Write to a file
+    void WriteFile(string filePath)
+    {
+        StreamWriter sWriter;
+
+        if (!File.Exists(filePath))
+        {
+            sWriter = File.CreateText(Application.dataPath + "WriteFile.txt");
+        } else
+        {
+            sWriter = new StreamWriter(filePath);
+        }
+
+        for (int i = 0; i < writeList.Count; i++)
+        {
+            sWriter.WriteLine(writeList[i]);
+        }
+
+        sWriter.Close();
+    }
+
+    void AppendFile(string filePath)
+    {
+        StreamWriter sWriter;
+
+        if (!File.Exists(filePath))
+        {
+            sWriter = File.CreateText(Application.dataPath + "WriteFile.txt");
+        } else
+        {
+            sWriter = new StreamWriter(filePath, append: true);
+        }
+
+        for (int i = 0; i < writeList.Count; i++)
+        {
+            sWriter.WriteLine(writeList[i]);
+        }
+
+        sWriter.Close();
+    }
+
+}
