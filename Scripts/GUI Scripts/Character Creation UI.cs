3b0216ea64da485a325669e44ccfb2db80890254
Garrett Bowers <bowersg45@gmail.com>
2018-08-09 23:19:37 -0400

    Error correction
    Due to an error that can occur within the unity editor, I have changed the repository to only include the assets portion of the game.

---
+++ Scripts/GUI Scripts/Character Creation UI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCreationUI : MonoBehaviour {
+
+    private float speed;
+    [SerializeField]
+    private GameObject leftPanel;
+    [SerializeField]
+    private GameObject rightPanel;
+
+}
