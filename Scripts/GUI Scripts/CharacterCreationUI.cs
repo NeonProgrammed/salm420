1301fddb6defe30ee665273ba862eec649532e12
Garrett Bowers <bowersg45@gmail.com>
2018-08-26 22:53:57 -0400

    update
    update

--- Scripts/GUI Scripts/CharacterCreationUI.cs
+++ Scripts/GUI Scripts/CharacterCreationUI.cs
@@ -5,11 +5,29 @@ using UnityEngine.UI;
 
 public class CharacterCreationUI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public bool open = false;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>(); // Gets the animator component in all children components
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void Update()
+    {
     }
+
+    // If the info tab button is pressed
+    public void PressedTab ()
+    {
+        if (! open)
+        {
+            open = true;
+            anim.SetTrigger("OpenTab");
+        } else if (open) {
+            open = false;
+            anim.SetTrigger("CloseTab");
+        }
+    }
+
 }
