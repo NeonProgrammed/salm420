5b41bf4481820220d797d6027974afb2a1185a17
Garrett Bowers <bowersg45@gmail.com>
2018-09-11 01:34:20 -0400

    finishing info tab. Everything on the character creation is officially done

--- Scripts/GUI Scripts/CharacterCreationUI.cs
+++ Scripts/GUI Scripts/CharacterCreationUI.cs
@@ -7,8 +7,12 @@ public class CharacterCreationUI : MonoBehaviour {
 
     // Switching canvases
     public GameObject setupUI;
+    public Text setupUI_info;
     public GameObject attributesUI;
+    public Text attribUI_info;
     public GameObject backstoryUI;
+    public Text backstoryUI_info;
+
     public GameObject infoTab;
 
     // Vairbales for tab UI
@@ -16,7 +20,11 @@ public class CharacterCreationUI : MonoBehaviour {
     Animator anim;
 
     private void Start()
+
     {
+        setupUI_info.enabled = true;
+        attribUI_info.enabled = false;
+        backstoryUI_info.enabled = false;
 
         // For tab UI
         anim = infoTab.GetComponent<Animator>(); // Gets the animator component in all children components
@@ -43,22 +51,31 @@ public class CharacterCreationUI : MonoBehaviour {
     public void setupPressed()
     {
         setupUI.SetActive(true);
+        setupUI_info.enabled = true;
         attributesUI.SetActive(false);
+        attribUI_info.enabled = false;
         backstoryUI.SetActive(false);
+        backstoryUI_info.enabled = false;
     }
 
     public void attributesPressed()
     {
         setupUI.SetActive(false);
+        setupUI_info.enabled = false;
         attributesUI.SetActive(true);
+        attribUI_info.enabled = true;
         backstoryUI.SetActive(false);
+        backstoryUI_info.enabled = false;
     }
 
     public void backstoryPressed()
     {
         setupUI.SetActive(false);
+        setupUI_info.enabled = false;
         attributesUI.SetActive(false);
+        attribUI_info.enabled = false;
         backstoryUI.SetActive(true);
+        backstoryUI_info.enabled = true;
     }
 
 }
